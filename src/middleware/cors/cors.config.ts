50bb037e59425df6a34518ccdbdef9823728eb9a
claycot <claycotton@hotmail.com>
2024-04-20 21:35:45 -0400

    [claycot] Allow API requests in CORS.

--- src/middleware/cors/cors.config.ts
+++ src/middleware/cors/cors.config.ts
@@ -1,7 +1,7 @@
 const customCorsOptions = {
     origin: (origin, callback) => {
         const allowedOrigins = process.env.hasOwnProperty("ALLOWED_ORIGINS") ? process.env.ALLOWED_ORIGINS!.split(" ") : [];
-        if (allowedOrigins.indexOf(origin) !== -1) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
             callback(new Error(`Request from unauthorized origin: ${origin}`));
