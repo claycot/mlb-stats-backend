c59ccbc3c94e0e59b0e4dbc000804f999b05a1ef
claycot <claycotton@hotmail.com>
2024-04-23 08:35:10 -0400

    [claycot] Test bugfix for swapping pitchers on 3 outs.

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -160,8 +160,12 @@ export async function getGames(gameDate?: string) {
                 "top_bottom": game_2.liveData.linescore.inningHalf,
             };
             // 2. current pitchers
-            gameObj.teams.away["pitcher"] = game_2.liveData.linescore.isTopInning ? playerInfo[game_2.liveData.linescore.offense.pitcher.id] : playerInfo[game_2.liveData.linescore.defense.pitcher.id];
-            gameObj.teams.home["pitcher"] = game_2.liveData.linescore.isTopInning ? playerInfo[game_2.liveData.linescore.defense.pitcher.id] : playerInfo[game_2.liveData.linescore.offense.pitcher.id];
+            // have to check for 3 outs because it will swap the offense and defense players before setting the next inning,
+            // causing pitchers to swap teams (which is wrong!)
+            gameObj.teams.away["pitcher"] = game_2.liveData.linescore.isTopInning && game_2.liveData.linescore.outs !== 3 ?
+                playerInfo[game_2.liveData.linescore.offense.pitcher.id] : playerInfo[game_2.liveData.linescore.defense.pitcher.id];
+            gameObj.teams.home["pitcher"] = game_2.liveData.linescore.isTopInning && game_2.liveData.linescore.outs !== 3 ?
+                playerInfo[game_2.liveData.linescore.defense.pitcher.id] : playerInfo[game_2.liveData.linescore.offense.pitcher.id];
             // 3. current runners
             gameObj.state["diamond"] = {
                 "batter": false,
